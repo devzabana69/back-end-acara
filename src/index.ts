e989b3e24a16417faf2513ce1e033de67d9028af
zabana <devzabana@gmail.com>
2025-09-14 00:14:31 +0700

    membuat Validasi api register

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import router from "./routes/api";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const PORT = 3000;
 
